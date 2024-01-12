4e60328aeb18307c5b4d286457edace9aa4f36e5
Martin Holmes <m.holmes.de@gmail.com>
2024-01-12 07:51:13 +0100

    lazygit

---
+++ lua/holmes/plugins/lazygit.lua
@@ -0,0 +1,12 @@
+return {
+	"kdheepak/lazygit.nvim",
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	-- config = function()
+	-- 	local lazygit = require("lazygit")
+	--
+	-- 	lazygit.setup({})
+	-- end,
+}
