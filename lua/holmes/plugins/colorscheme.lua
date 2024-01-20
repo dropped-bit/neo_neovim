0f9526bde620e57e6d7d90d0cac734e5e1b58e37
Martin Holmes <m.holmes.de@gmail.com>
2024-01-20 21:02:09 +0100

    theme changes

--- lua/holmes/plugins/colorscheme.lua
+++ lua/holmes/plugins/colorscheme.lua
@@ -1,31 +1,32 @@
 return {
-	-- "navarasu/onedark.nvim",
-	-- priority = 1000,
-	-- config = function()
-	-- 	require("onedark").setup({
-	-- 		style = "darker",
-	-- 		transparent = true,
-	-- 		lualine = {
-	-- 			transparent = true,
-	-- 		},
-	-- 	})
-	-- 	require("onedark").load()
-	"catppuccin/nvim",
+	"navarasu/onedark.nvim",
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = true,
-			-- lualine = {
-			-- 	transparent = true,
-			-- },
+		require("onedark").setup({
+			style = "dark",
+			transparent = false,
+			lualine = {
+				transparent = true,
+			},
 		})
-		require("catppuccin").load()
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		require("onedark").load()
+		-- "catppuccin/nvim",
+		-- priority = 1000,
+		-- config = function()
+		-- 	require("catppuccin").setup({
+		-- 		flavour = "mocha",
+		-- 		transparent_background = true,
+		-- 		-- lualine = {
+		-- 		-- 	transparent = true,
+		-- 		-- },
+		-- 	})
+		-- 	require("catppuccin").load()
+		-- 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- 	-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		-- 	-- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+		-- 	-- vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
+		-- 	-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		-- end,
 	end,
 }
