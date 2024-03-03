0a4f488dd1e71ad52fbe6113a5e77e4311ee37dd
Martin Holmes <m.holmes.de@gmail.com>
2024-03-03 21:59:25 +0100

    some updates inspired by kickstart which-key + telescope

--- lua/holmes/core/options.lua
+++ lua/holmes/core/options.lua
@@ -32,6 +32,15 @@ vim.opt.updatetime = 50
 vim.diagnostic.config({
 	float = { border = "rounded" },
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 -- disable netrw at the very start of your init.lua (strongly advised)
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
