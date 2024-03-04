2fd27e467c1efed0aabf37811a10679cff2edfe5
Martin Holmes <m.holmes.de@gmail.com>
2024-03-04 08:18:21 +0100

    trying to integrate kickstart

--- lua/holmes/plugins/general.lua
+++ lua/holmes/plugins/general.lua
@@ -2,7 +2,15 @@ return {
 	"christoomey/vim-tmux-navigator",
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"AckslD/swenv.nvim",
+	-- NOTE: DO THE FOLLOWING HERE
 	-- colorschemes
 	-- "navarasu/onedark.nvim",
 	-- "catppuccin/nvim",
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
